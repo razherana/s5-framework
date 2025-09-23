dd179ae80b6c85eb16ee268f06b425cedbcd0cf1
razherana <razherana@gmail.com>
2025-09-23 17:53:13 +0300

    Initialized project

---
+++ scripts/jar.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/sh
+
+cd /home/razherana/Documents/S5/MrNaina-Framework/framework/ || exit
+
+if [ -d out ]; then
+  rm -rf out
+fi
+
+mkdir out
+find src/main -name "*.java" -print0 | xargs -0 javac -parameters -d out
+
+cd out || exit
+jar cf ../framework.jar .
+cd .. || exit
+rm -rf out
+
+echo "Jar created at framework/framework.jar"

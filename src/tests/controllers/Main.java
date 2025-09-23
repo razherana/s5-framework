2ecd39e3e87a413865a4eaddb8412e587261346a
razherana <razherana@gmail.com>
2025-09-23 17:56:39 +0300

    Haricot finder and controller test

---
+++ src/tests/controllers/Main.java
@@ -0,0 +1,9 @@
+package controllers;
+
+import mg.razherana.framework.finders.haricots.HaricotFinder;
+
+public class Main {
+  public static void main(String[] args) {
+    HaricotFinder.printHaricotInfo("controllers");
+  }
+}
---
+++ src/tests/controllers/TestController.java
@@ -0,0 +1,7 @@
+package controllers;
+
+import mg.razherana.framework.annotations.controllers.Controller;
+
+@Controller("/test")
+public class TestController {
+}
